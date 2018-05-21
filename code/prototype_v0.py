7366bc5950f38b724bfe6a5cbab5ffc7f4dd4fe6
CiroGamboa <ciro.gamboa.2013@upb.edu.co>
2018-05-21 13:38:19 -0500

    Calculo de eigenvalues, es necesario interpretar para obtener DOP

--- code/prototype_v0.py
+++ code/prototype_v0.py
@@ -8,53 +8,36 @@ A partir del prototipo 1 y en adelante, vinculados con sprints
 Se tendra todo documentado de forma estandar
 Toda la documentacion debe ser en ingles
 """
-
 # Import the libraries
 from obspy import read
+from tools import TelluricoTools
+from obspy.signal.polarization import eigval
 
 # Read seismograms
 st = read('IOfiles/2013_06_2013-06-18-0559-59M.COL___261')
 
-# Get one trace
-tr = st[20]
 
-# Access Meta Data
-print(tr.stats)
-
-# Access Waveform Data
-print(tr.data)
-
-# Plot Complete Data
-st.plot( )
-
-# Plot single trace
-tr.plot()
-
-## Compare station info between Sfile and Waveform
-# Retrieve stations in Waveform
-
-# In this example there are 39 different stations in the Sfile
-waveform_stations = []
+# Get a non-zero trace
+test_trace = None
+index = 0
 for trace in st:
-    waveform_stations.append(trace.stats.station)
-    print(trace.stats.station)
+    if(TelluricoTools.check_trace(trace)):
+        test_trace = [trace,st[index+1], st[index+2]]
+        break
+    index = index + 1
     
     
-# Remove duplicates in list
-def remove_duplicates(values):
-    output = []
-    seen = set()
-    for value in values:
-        # If value has not been encountered yet,
-        # ... add it to both list and set.
-        if value not in seen:
-            output.append(value)
-            seen.add(value)
-    return output
 
-# Remove duplicates from this list.
-waveform_stattions_filtered = remove_duplicates(waveform_stations)
-print(waveform_stattions_filtered)
+# It is necessary to make a table describing the data convention
+# PREGUNTA PARA EDWAR, CUALES SERIAN LOS MEJORES PARAMETROS PARA CALCULAR
+# EL DOP DE LA SEÑAL.... VER REFERENCIA DE OBSPY: https://docs.obspy.org/packages/autogen/obspy.signal.polarization.eigval.html#obspy.signal.polarization.eigval
+
+DOP = eigval(datax = test_trace[0],
+             datay = test_trace[1],
+             dataz = test_trace[2],
+             fk = [1, 1, 1, 1, 1],
+             normf = 1.0)
+    
 
 
 
